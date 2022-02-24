385abe61c59cf33879c6bd485d075333dd1a5fd7
YossiBenZaken <yossibz@gmail.com>
2022-02-24 11:21:15 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yosef Ben Zaken on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
